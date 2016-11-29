ed47424d9e01955652d9f9de4d490356904423a6
victor <vico.16c@gmail.com>
2016-11-29 12:58:56 -0500

    resize lambda asyncrono

--- proc.js
+++ proc.js
@@ -1,55 +1,65 @@
 /**
  * Created by victor on 29/11/16.
  * https://www.npmjs.com/package/aws-lambda-invoke
+ * http://stackoverflow.com/questions/9437581/node-js-amazon-s3-how-to-iterate-through-all-files-in-a-bucket
+ * resize bucket libero-media-v
+ * ejecutar :  node proc.js 
  */
 var aws = require('aws-sdk');
 aws.config.update({region:'us-east-1'});
-
 // prueba en tu pc -.-
 //aws.config.update({region:'us-east-1',secretAccessKey:'dddddddddd',accessKeyId:'rrrrrrrrrrrrrrrreeeeeeeeeeeeeeeeeeeee'});
-
 const lambda = require('aws-lambda-invoke');
+var s3 = new aws.S3();
 
-var event = {
-  "Records": [
-    {
-      "eventVersion": "2.0",
-      "eventSource": "aws:s3",
-      "awsRegion": "us-east-1",
-      "eventTime": "2016-11-28T04:03:39.359Z",
-      "eventName": "ObjectCreated:Put",
-      "userIdentity": {
-        "principalId": "AWS:AIDAJ5KU2XIUKIDGRVMKY"
-      },
-      "requestParameters": {
-        "sourceIPAddress": "179.7.214.174"
-      },
-      "responseElements": {
-        "x-amz-request-id": "DDAF87FED7B7F2F0",
-        "x-amz-id-2": "sTc+loSeL5BqSGrFKTZpksWCoYm4upldDuR2mCEh+iS4EYe/UeQf0QFimeMiaPcU"
-      },
-      "s3": {
-        "s3SchemaVersion": "1.0",
-        "configurationId": "3d4e2e55-b1d5-41cb-a369-548aed5935df",
-        "bucket": {
-          "name": "libero-media-v",
-          "ownerIdentity": {
-            "principalId": "A2NV1NQXJVK9VY"
-          },
-          "arn": "arn:aws:s3:::libero-media-v"
-        },
-        "object": {
-          "key": "libero/imagen/2016/01/22/23081870.jpg",
-          "size": 84034,
-          "eTag": "99a81f8bf2b4b28adebb82e849d319d2",
-          "sequencer": "00583BAC9B43BCC3D3"
-        }
-      }
-    }
-  ]
+var event = require('./event.json');
+var srcBucket = event.Records[0].s3.bucket.name;
+
+
+var params = {
+    Bucket: srcBucket,
+    Prefix: 'libero/imagen/2016/01/01/',
+    EncodingType: 'url',
+    //Delimiter : 'imagen/2010/05/05'
+   // Sufix: '.jpg'
 };
 
+var s3DataContents = [];    // Single array of all combined S3 data.Contents
+
+s3ListObjects(params);
+
+function s3ListObjects(params) {
+    s3.listObjects(params, function(err, data) {
+        if (err) {
+            console.log("listS3Objects Error:", err);
+        } else {
+            var contents = data.Contents;
+            cant = 0;
+			contents.forEach(function(entry) {
+				var key = entry.Key;
+				validationIsImage = (/\.(jpg|png)$/i).test(key)
+				if(validationIsImage == true){
+					console.log(key);
+					event.Records[0].s3.object.key = key;
+					lambda.invokeAsync('glrGenerateThumbnails-dev-resize', event).then(result => {
+					    console.log(result);
+					});
+				}
+
+			    cant = cant +1 ;
+			});
+			console.log("Cantidad de procesados (limit 1000): "+cant);
+        }
+    });
+}
+
+
+
+
+/*
 lambda.invokeAsync('glrGenerateThumbnails-dev-resize', event).then(result => {
     console.log(result);
 //=> '{"foo": "baz"}'
 });
+
+*/
